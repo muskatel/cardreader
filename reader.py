ae408e034915c3da8f95bdd91c8f5c4d257804ea
Greg Linklater <greglink49@gmail.com>
2015-01-21 12:02:47 +0200

    Changed the readme

--- reader.py
+++ reader.py
@@ -8,13 +8,12 @@ def main():
     read = 0
     lcd.init()
     nfc.init()
-    lcd.screen_write('Ready...')
+    lcd.screen_write('Ready...', '')
     
     while True:
         card = nfc.read_card()
-        read += 1
+	read += 1
         lcd.screen_write('Card: ' + card, 'Count: ' + str(read))
-        
 
 if __name__ == '__main__':
     main()
